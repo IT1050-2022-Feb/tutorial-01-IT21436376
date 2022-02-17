942a9120ce2c738dd99e8aff85f6ae07f2eaf562
IT21436376 <99354033+IT21436376@users.noreply.github.com>
2022-02-18 00:29:27 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -5,15 +5,45 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+//Function Declaration
+int maximum(int num1, int num2);
+int minimum(int num1,int num2);
+int multiply(int num1,int num2);
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+int main() 
+{
+   int num1, num2;
+   printf("Enter a value- no 1 : ");
+   scanf("%d", &num1);
+   printf("Enter a value- no 2 : ");
+   scanf("%d", &num2);
+   printf("%d ", minimum(num1, num2));//Funtion Call
+   printf("%d ", maximum(num1, num2));
+   printf("%d ", multiply(num1, num2));
    return 0;
+}
+
+//Function Implimentation
+int maximum(int num1, int num2){
+  int max=0;
+  if(num1>num2)
+    max=num1;
+  if(num1<num2)
+    max=num2;
+    
+return max;
+
+}
+int minimum(int num1,int num2){
+  int min=0;
+  if(num1>num2)
+    min=num2;
+  if(num1<num2)
+    min=num1;
+    
+return min;
+}
+
+int multiply(int num1,int num2){
+return num1*num2;
 }
